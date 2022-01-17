bf6b8f9382fb4c41e36cb2edd435dde5f816b230
Luca Bonamini <lucabonamini28@gmail.com>
2022-01-17 22:24:31 +0100

    refactor(cubic_spline_planner): changes according to clang-tidy report

--- planning/cubic_spline_planner/include/cubic_spline_planner/cubic_spline_planner.h
+++ planning/cubic_spline_planner/include/cubic_spline_planner/cubic_spline_planner.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include "utilities/math.h"
+
 #include <array>
 #include <eigen3/Eigen/Core>
 #include <vector>
 
 namespace planning {
+class Spline2D;
 class Spline {
 public:
   Spline() = default;
-  Spline(const std::vector<double> &x_, const std::vector<double> &y_);
-
+  Spline(const std::vector<double> &x, const std::vector<double> &y);
+private:
+  friend class Spline2D;
   std::vector<double> x;
   std::vector<double> y;
   int nx{0};
@@ -33,24 +37,19 @@ public:
 
 class Spline2D {
 public:
-  // TODO(lucabonamini): manage with friendship
-  Spline sx;
-  Spline sy;
-  std::vector<double> s;
-
   Spline2D(const std::vector<double> &x, const std::vector<double> &y);
-
   std::array<double, 2> calc_position(double s_t);
-
+  std::vector<double> s;
+private:
   double calc_curvature(double s_t);
 
   double calc_yaw(double s_t);
 
   std::array<double, 2> calcCartesianCoordinates(const double &s,
                                                  const double &d);
-
-private:
   static std::vector<double> calc_s(const std::vector<double> &x,
                                     const std::vector<double> &y);
+  Spline sx;
+  Spline sy;
 };
 } // namespace planning
