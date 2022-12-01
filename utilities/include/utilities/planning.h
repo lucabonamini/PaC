6cef963e8a4ea0bcf15a171fa75c955127326d4e
Luca Bonamini <lucabonamini28@gmail.com>
2022-12-01 16:01:36 +0100

    Change Path to Traj in planning utilities

--- utilities/include/utilities/planning.h
+++ utilities/include/utilities/planning.h
@@ -16,11 +16,11 @@ namespace planning {
  */
 void findClosestIndex(size_t &index,
                       const ::types::Point &point,
-                      const ::types::Path &path) {
+                      const ::types::Traj &path) {
   double closest_distance = std::numeric_limits<double>::max();
   for (size_t i = index; i < path.size(); i++) {
-    double g_x = path.at(i).point.x;
-    double g_y = path.at(i).point.y;
+    double g_x = path.at(i).x;
+    double g_y = path.at(i).y;
     double current_closest_distance =
         pow((point.x - g_x), 2) + pow((point.y - g_y), 2);
     if (current_closest_distance < closest_distance) {
